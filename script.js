808a3aad37ac0ede58b5c97b27ee1160c9e6ddea
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2019-10-27 17:25:58 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(function(){
-   // #にダブルクォーテーションが必要
    $('a[href^="#"]').click(function() {
       var speed = 400;
       var href= $(this).attr("href");
@@ -10,24 +9,19 @@ $(function(){
    });
 });
 $(function() {
-   // 省略...
    $(window).scroll(function() {
-     // 「TOPに戻る」ボタンを取得します。
+
      var $toTopButton = $('#scroll_to_top');
  
-     // 縦にどれだけスクロールしたかを取得します。
      var scrollTop = $(this).scrollTop();
  
-     // ウィンドウの縦幅を取得します。
      var windowHeight = $(this).height();
  
      if (scrollTop >= windowHeight) {
-       // ウィンドウの縦幅以上にスクロールしていた場合、
-       // 「TOPに戻る」ボタンを表示します。
+
        $toTopButton.show();
      } else {
-       // ウィンドウの縦幅以上にスクロールしていない場合、
-       // 「TOPに戻る」ボタンを隠します。
+
        $toTopButton.hide();
      }
    });
